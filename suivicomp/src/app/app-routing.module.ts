51304dd71ae6fa2e14eea7be65ac507c2cb2fc53
aymane <Aymane.Rizke.Etu@univ-lemans.fr>
2020-03-17 16:45:18 +0100

    IHM page quiz-etudiant2

--- suivicomp/src/app/app-routing.module.ts
+++ suivicomp/src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},  {
+  { path: '', redirectTo: 'quiz-etudiant2', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
     path: 'quiz-etudiant2',
     loadChildren: () => import('./quiz-etudiant2/quiz-etudiant2.module').then( m => m.QuizEtudiant2PageModule)
   },
