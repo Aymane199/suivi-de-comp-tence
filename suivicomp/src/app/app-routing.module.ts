71f35a1bc3ca4eb6fd28ad994007bde9d1073539
Aristide <aristide.laignel@outlook.fr>
2020-03-20 14:42:08 +0100

    Lien entre les deux pages de connexion

--- suivicomp/src/app/app-routing.module.ts
+++ suivicomp/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'connexion2', pathMatch: 'full' },
+  { path: '', redirectTo: 'connexion', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   {
     path: 'quiz-etudiant2',
