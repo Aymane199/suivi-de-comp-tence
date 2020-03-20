ae3476996b2af55271439232cd5b383c465dc881
Aymane <aymanerizke199@gmail.com>
2020-03-20 16:26:33 +0100

    add service test & navigation between questions

--- suivicomp/src/app/quiz-etudiant2/quiz-etudiant2.page.ts
+++ suivicomp/src/app/quiz-etudiant2/quiz-etudiant2.page.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TestService} from '../services/test.service';
 
 @Component({
-  selector: 'app-quiz-etudiant2',
-  templateUrl: './quiz-etudiant2.page.html',
-  styleUrls: ['./quiz-etudiant2.page.scss'],
+    selector: 'app-quiz-etudiant2',
+    templateUrl: './quiz-etudiant2.page.html',
+    styleUrls: ['./quiz-etudiant2.page.scss'],
 })
 export class QuizEtudiant2Page implements OnInit {
 
-  constructor() { }
+    testService: TestService;
+    currentQuestionIndex: number;
+    questionCount: number;
+    currentQuestion: string;
+    currentResponses: string[];
 
-  ngOnInit() {
+    constructor(testService: TestService) {
+
+        this.testService = testService;
+        this.questionCount = testService.questions.length;
+        this.currentQuestionIndex = 0;
+        this.currentQuestion = this.testService.questions[this.currentQuestionIndex];
+        this.currentResponses = this.testService.responses[this.currentQuestionIndex].split("-");
+
+    }
+
+    ngOnInit() {
+    }
+
+  questionSuivant() {
+
+    this.currentQuestionIndex++;
+    this.currentQuestion = this.testService.questions[this.currentQuestionIndex];
+    this.currentResponses = this.testService.responses[this.currentQuestionIndex].split("-");
   }
 
+    quizTerminer() {
+    }
 }
