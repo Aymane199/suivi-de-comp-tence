c2bf20b5b2ea0c4e7b0e8c296fd0f6b2bd14739f
Hugo <hugo_valentin.etu@univ-lemans.fr>
2020-03-24 10:15:06 +0100

    C'est terminé pour le joueur quiz3

--- suivicomp/src/app/quizz3/quizz3.page.ts
+++ suivicomp/src/app/quizz3/quizz3.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {TestService} from '../services/test.service';
+import {ProfilService} from '../profil.service';
 
 @Component({
   selector: 'app-quizz3',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Quizz3Page implements OnInit {
 
-  constructor() { }
+  prenom:string;
+  nbQuestion:number;
+  nbTrue:number;
+  nbFalse:number;
+  points:number;
+  constructor(private testService: TestService, private profilService : ProfilService) { }
 
   ngOnInit() {
+    this.prenom=this.profilService.Prenom;
+    this.nbQuestion=this.profilService.reponse.length;
+    this.nbTrue=0;
+    this.nbFalse=0;
+    for(let i=0;i<this.profilService.reponse.length;i++){
+      if(this.profilService.reponse[i]==1){
+        this.nbTrue++;
+      }
+      else{
+        this.nbFalse++;
+      }
+    }
+    this.points=this.nbTrue*1000;
   }
 
 }
