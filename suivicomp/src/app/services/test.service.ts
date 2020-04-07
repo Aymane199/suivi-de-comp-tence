9ca94b3138f28e73d81648787eff0eb842c17610
Aymane <aymanerizke199@gmail.com>
2020-04-07 10:52:43 +0200

    traduction en français

--- suivicomp/src/app/services/test.service.ts
+++ suivicomp/src/app/services/test.service.ts
@@ -8,23 +8,23 @@ export class TestService {
   //question[0] : response[0][1]-response[0][1]-response[0][2]-response[0][3] : correctRes[0]
 
   questions: string[] = [
-      'How de you call a fucntion named myFunction ?',
+      'Comment vous appelez une fonction nommée myFunction?',
 
-      'what is the correct Javascript syntax to write hello word ?',
+      'Quelle est la syntaxe Javascript pour écrire bonjour?',
 
-      'how to write an IF statement in JavaScript ?',
+      'Comment écrire une instruction IF en JavaScript?',
 
-      'Where is the correct place to insert a JavaScript?'
+      'Où est le bon endroit pour insérer un code JavaScript?'
   ];
 
   responses: string[] = [
         'myFunction()-call function my function-call myfunction-myfunction',
 
-        'echo "hello word"-echo "hello word-dowument.write("helllo word")-respose.write("hello word")',
+        'echo "hello world"-echo "hello world-document.write("hello world")-response.write("hello world")',
 
         'if i = 5 then-if i = 5-if i== 5 then-if(i == 5)',
 
-        'The head section-The body section-Both the head section and the body section are correct'
+        'La section tête-La section corps-La section tête et la section corps sont correctes'
   ];
 
   correctResponse: number[] = [
